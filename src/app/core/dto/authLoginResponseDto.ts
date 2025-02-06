9c428700bd51e515856e1328bf803563d1771dd1
Guillermo-git <avalosguillermo1996@gmail.com>
2025-02-06 15:00:01 -0600

    ahuth 2

--- src/app/core/dto/authLoginResponseDto.ts
+++ src/app/core/dto/authLoginResponseDto.ts
@@ -1,3 +1,4 @@
 export interface AuthLoginResponseDto {
+    
     jwt: string;
 }
--- src/app/feature/auth/auth-routing.module.ts
+++ src/app/feature/auth/auth-routing.module.ts
@@ -10,7 +10,8 @@ const routes: Routes = [
   },
   {
     path:"registro",
-    component:RegistroComponent
+    component:RegistroComponent,
+    
   }
 ];
 
